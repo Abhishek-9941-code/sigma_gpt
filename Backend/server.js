4523139b6a0bb25ff6d99df26a7c89136e27dee7
Abhishek <abhisheksaini9941@gmail.com>
2026-08-14 21:30:59 +0530

    final fix cors problem

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 app.use(
     cors({
-        origin: "http://127.0.0.1:5500",
+        origin: ["http://127.0.0.1:5500","https://sigma-gpt-livid.vercel.app/"],
         credentials: true
     })
 );
